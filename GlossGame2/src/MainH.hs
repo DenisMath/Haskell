e15c7295723ad1f56a4d919122b310a5d27df35e
DenisMath <denis.morozov178@gmail.com>
2022-04-21 23:58:24 +0300

    new commit

--- GlossGame2/src/MainH.hs
+++ GlossGame2/src/MainH.hs
@@ -3,7 +3,9 @@ import Lib
 import Graphics.Gloss
 import Graphics.Gloss.Data.ViewPort
 import Graphics.Gloss.Interface.IO.Game
-import Space (Action)
+import Space 
+
+type World_ = World
 
 class PictureClass a where
     toPicture :: a -> Picture
@@ -13,11 +15,16 @@ instance PictureClass World where
 
 data ActionR = MouseAct Float Float | KeyAct String | DefaultAct
 
+instance Space World where
+    initS = World [ObjType {coord = (0,0), imageType = "Red"}, ObjType {coord = (1,1), imageType = "Black"}]
+    nextS (World x) = World $ map (\t -> ObjType {coord = (fst (coord t) + 1, snd (coord t) + 1), imageType = imageType t}) x
+
+
 windowDisplay :: Display
 windowDisplay = InWindow "Window" (200, 200) (10, 10)
 
-initObjList :: World
-initObjList= World [ObjType {coord = (0,0), imageType = "Red"}, ObjType {coord = (1,1), imageType = "Black"}]
+initWorld :: World_
+initWorld = initS
 
 drawingFunc :: (PictureClass a) => a -> Picture
 drawingFunc = toPicture
@@ -30,7 +37,7 @@ eventToAction (EventKey (SpecialKey KeyLeft) Down _ _) = KeyAct "KeyLeft"
 eventToAction (EventKey (MouseButton LeftButton) Down _ (x', y')) = MouseAct x' y'
 eventToAction _ = DefaultAct
 
-inputHandler :: Event -> World -> World
+inputHandler :: Event -> World_ -> World_
 inputHandler (EventKey (SpecialKey KeyUp) Down _ _) x = x
 inputHandler (EventKey (SpecialKey KeyDown) Down _ _) x = x
 inputHandler (EventKey (SpecialKey KeyRight) Down _ _)  x = x
@@ -38,8 +45,8 @@ inputHandler (EventKey (SpecialKey KeyLeft) Down _ _) x = x
 inputHandler (EventKey (MouseButton LeftButton) Down _ (x', y')) (World x) = World $ defaultObj (round x') (round y'):x
 inputHandler _ w = w
 
-updateFunc :: Float -> World -> World
-updateFunc _ l = l
+updateFunc :: Float -> World_ -> World_
+updateFunc _ = nextS 
 
 
 
